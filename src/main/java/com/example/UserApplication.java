ae41e96c182e4078b8c658b77d2bdbd3aad45986
Salman <salmankhandu@gmail.com>
2020-05-22 00:58:35 +0530

    add mapping entity to dto with sort

--- src/main/java/com/example/UserApplication.java
+++ src/main/java/com/example/UserApplication.java
@@ -1,7 +1,9 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 
@@ -10,6 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.mapper.Role;
+import com.example.mapper.RoleDTO;
 import com.example.mapper.User;
 import com.example.mapper.UserDTO;
 import com.example.mapper.UserMapper;
@@ -27,20 +30,80 @@ public class UserApplication {
 	@PostConstruct
 	public void test() {
 
+		List<User> userList = new ArrayList<>();
 		User user = new User();
 		user.setAge("28");
-		user.setName("Alex");
-	    List<Role> roles = new ArrayList<>();
-	    Role role = new Role();
-	    role.setId("1");
-	    role.setName("ROLE_ADMIN");
+		user.setName("Zem");
+		List<Role> roles = new ArrayList<>();
+		Role role = new Role();
+		role.setId("1");
+		role.setName("SUPER_ADMIN");
+		roles.add(role);
+
+		role = new Role();
+		role.setId("2");
+		role.setName("USER");
+		roles.add(role);
+
+		role = new Role();
+		role.setId("3");
+		role.setName("ADMIN");
 		roles.add(role);
 		user.setRoles(roles);
+
+		userList.add(user);
 		
-		UserDTO userDTO = this.userMapper.toDTO(user);
-		System.out.println(user.getAge());
-		System.out.println(user.getName());
-		userDTO.getRolesDTOs().forEach(roleDTO-> System.out.println(roleDTO.getId()+ "" + roleDTO.getName()));
+		
+		user = new User();
+		user.setAge("27");
+		user.setName("Cux");
+		roles = new ArrayList<>();
+		role = new Role();
+		role.setId("1");
+		role.setName("STORE");
+		roles.add(role);
+
+		role = new Role();
+		role.setId("1");
+		role.setName("MANAGER");
+		roles.add(role);
+		
+		role = new Role();
+		role.setId("1");
+		role.setName("REGION_MANAGER");
+		roles.add(role);
+		
+		role = new Role();
+		role.setId("1");
+		role.setName("ADMIN");
+		roles.add(role);
+		
+		user.setRoles(roles);
+		userList.add(user);
+
+		user = new User();
+		user.setAge("25");
+		user.setName("Alex");
+		user.setRoles(roles);
+		userList.add(user);
+
+		//mapping entity to dto with sorting
+		List<UserDTO> sortedList = userList.stream().map(u1 -> this.userMapper.toDTO(u1))
+				.sorted(Comparator.comparing(UserDTO::getName))
+				.map(this::sortRole).collect(Collectors.toList());
+		
+		//print result to check
+		sortedList.stream().forEach(u1 ->{
+			String roleString = u1.getRolesDTOs().stream().map(rrr-> rrr.getName()).collect(Collectors.joining(","));
+			System.out.println(u1.getName() +" ==> " + roleString + "\n\n");
+		});
+	}
+
+	private UserDTO sortRole(UserDTO userDTO) {
+		userDTO.setRolesDTOs(userDTO.getRolesDTOs().stream().sorted(Comparator.comparing(RoleDTO::getName))
+				.collect(Collectors.toList()));
+		return userDTO;
+
 	}
 
 }
